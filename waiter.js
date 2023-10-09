0384179f44eeb143e7ef92ff92f54c4e2428f54a
Yamisa11 <majijayamisa@gmail.com>
2023-10-09 11:29:25 +0200

    update

--- waiter.js
+++ waiter.js
@@ -1,8 +1,10 @@
 export default function WaiterAvailability(database) {
 
-  
+  async function addShift(theWaiterId,theDaysId){
+    await database.createRoster(theWaiterId,theDaysId)
+  }
 
     return{
-    
+    addShift
     }
 }
