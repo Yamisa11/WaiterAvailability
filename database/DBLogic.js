0de2f8cef585e149530712b6442f1e12fb67de58
Yamisa11 <majijayamisa@gmail.com>
2023-10-09 11:25:30 +0200

    update on db logic

--- database/DBLogic.js
+++ database/DBLogic.js
@@ -11,25 +11,20 @@ export default function WaiterDBLogic(database) {
         let weekday_id = await database.one('SELECT id FROM weekdays WHERE weekday=$1', [day])
         return weekday_id;
     }
-    async function createRoster(waiter_id, weekday_id) {
-        try {
-            await database.none('INSERT INTO schedule (waiter_id, weekday_id) VALUES ($1, $2)', [waiter_id, weekday_id]);
-            return 'Days added successfully!';
-        } catch (error) {
-            return false
-        }
+    async function createRoster(waiterid, weekdayid) {
+        await database.none('INSERT INTO shifts (waiterid, weekdayid) VALUES ($1, $2)', [waiterid, weekdayid]);
     }
 
-    async function joinQuery() {
-        const daysQuery = `
-                    SELECT  * FROM schedule
-                    JOIN weekdays ON weekdays.id = schedule.weekday_id
-                    JOIN waiters ON waiters.id = schedule.waiter_id
-                `;
+    // async function joinQuery() {
+    //     const daysQuery = `
+    //                 SELECT  * FROM schedule
+    //                 JOIN weekdays ON weekdays.id = schedule.weekday_id
+    //                 JOIN waiters ON waiters.id = schedule.waiter_id
+    //             `;
 
-        const results = await database.any(daysQuery);
-        return results;
-    }
+    //     const results = await database.any(daysQuery);
+    //     return results;
+    // }
 
 
 
