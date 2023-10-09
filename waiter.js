1c496b2dd6c7f2adb46f375f25cb2783376a81d9
Yamisa11 <majijayamisa@gmail.com>
2023-10-09 11:12:08 +0200

    update on factory function

--- waiter.js
+++ waiter.js
@@ -1,50 +1,8 @@
 export default function WaiterAvailability(database) {
 
-    async function setUsers(username,theRole){
-        await database.createUser(username,theRole)
-    }
-
-    async function getMondayRoster(){
-        let monRoster = await database.getEmployees('MONDAY')
-        return monRoster
-    }
-
-    async function getTuesdayRoster(){
-        let tuesRoster = await database.getEmployees('TUESDAY')
-        return tuesRoster
-    }
-
-    async function getWednesdayRoster(){
-        let wedRoster = await database.getEmployees('WEDNESDAY')
-        return wedRoster 
-    }
-
-    async function getThursdayRoster(){
-        let thursRoster = await database.getEmployees('THURSDAY')
-        return thursRoster
-    }
-
-    async function getFridayRoster(){
-        let friRoster = await database.getEmployees('FRIDAY')
-        return friRoster 
-    }
-
-    async function getSaturdayRoster(){
-        let satRoster = await database.getEmployees('SATURDAY')
-        return satRoster  
-    }
-    async function updateRoster(theDay,employee){
-        await database.updateEmployeeRoster(theDay,employee)
-    }
+  
 
     return{
-        setUsers,
-        getFridayRoster,
-        getMondayRoster,
-        getSaturdayRoster,
-        getThursdayRoster,
-        getWednesdayRoster,
-        getTuesdayRoster,
-        updateRoster
+    
     }
 }
