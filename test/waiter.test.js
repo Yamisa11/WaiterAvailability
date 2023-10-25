8552ac39f49edad8e5d2f16047504cb1487b39cd
Yamisa11 <majijayamisa@gmail.com>
2023-10-25 04:58:57 +0200

    added tests

---
+++ test/waiter.test.js
@@ -0,0 +1,36 @@
+import assert from "assert"
+import waiterDBLogic from "../database/DBLogic.js";
+import pgPromise from 'pg-promise';
+
+const connectionString = process.env.DATABASE_URL || 
+
+const db = pgPromise()(connectionString);
+describe('Waiter Availability App database tests', () => {
+    let registrationDBLogic = waiterDBLogic(db)
+
+    beforeEach(async () => {
+        await registrationDBLogic.reset()
+    })
+
+    it('should check if an existing waiter with a given username exists', async () => {
+        const result = await dbLogic.checkExistingWaiter('Keesha');
+        assert.deepStrictEqual(result, []);
+      });
+    
+      it('should get the weekday ID for a given day', async () => {
+        const weekdayId = await dbLogic.getWeekdayId('Monday');
+        assert.strictEqual(weekdayId, 1); 
+      });
+    
+      it('should get a waiter ID for a given username', async () => {
+        const waiterId = await dbLogic.getWaiterId('Kwane');
+        assert.strictEqual(waiterId, 1); 
+      });
+    
+      it('should create a roster entry', async () => {
+        await dbLogic.createRoster(1, 1); 
+      
+      });
+    
+    
+})
